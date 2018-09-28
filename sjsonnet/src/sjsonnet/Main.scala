e6bc055dab0b2390df42127ba3fa5b18bc9ada4d
Li Haoyi <haoyi.sg@gmail.com>
2018-09-28 22:28:15 +0800

    tweak-cli

--- sjsonnet/src/sjsonnet/Main.scala
+++ sjsonnet/src/sjsonnet/Main.scala
@@ -9,18 +9,25 @@ object Main {
         System.exit(1)
       case Right((config, leftover)) =>
         leftover match{
-          case List(file) =>
-            val path = ammonite.ops.Path(file, ammonite.ops.pwd)
-            val interp = new Interpreter(new Parser, Scope.standard(path, Nil))
-            interp.interpret(path) match{
-              case Left(errMsg) =>
-                System.err.println(errMsg)
+          case file :: rest =>
+            Cli.groupArgs(rest, Cli.genericSignature, config) match{
+              case Left(err) =>
+                println(err)
+                println(Cli.help)
                 System.exit(1)
-              case Right(materialized) =>
-                val str = materialized.render(indent = 4)
-                config.outputFile match{
-                  case None => println(str)
-                  case Some(f) => ammonite.ops.write(ammonite.ops.Path(f, ammonite.ops.pwd), str)
+              case Right((config, rest)) =>
+                val path = ammonite.ops.Path(file, ammonite.ops.pwd)
+                val interp = new Interpreter(new Parser, Scope.standard(path, Nil))
+                interp.interpret(path) match{
+                  case Left(errMsg) =>
+                    System.err.println(errMsg)
+                    System.exit(1)
+                  case Right(materialized) =>
+                    val str = materialized.render(indent = 4)
+                    config.outputFile match{
+                      case None => println(str)
+                      case Some(f) => ammonite.ops.write(ammonite.ops.Path(f, ammonite.ops.pwd), str)
+                    }
                 }
             }
 
