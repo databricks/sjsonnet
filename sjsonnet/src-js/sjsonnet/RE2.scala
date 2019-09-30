5dab570ac008d77bac71f1384c325f0f9ad09452
Eugene Berman <eugene.berman@modusbox.com>
2019-09-30 11:14:25 -0700

    Should be re2, not re2.js

--- sjsonnet/src-js/sjsonnet/RE2.scala
+++ sjsonnet/src-js/sjsonnet/RE2.scala
@@ -3,7 +3,7 @@ package sjsonnet
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
-@JSImport("re2.js", "RE2")
+@JSImport("re2", "RE2")
 @js.native
 class RE2(val pattern: String) extends js.Object {
   def `match`(str: String): Boolean = js.native
