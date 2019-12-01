3a8a7ae168fc4b25c8299e7cc8d79d584c6d5f1e
Rohit Agrawal <rohit.agrawal@databricks.com>
2019-12-01 10:24:51 -0800

    Added GZip Encode Feature

--- sjsonnet/src-js/sjsonnet/Platform.scala
+++ sjsonnet/src-js/sjsonnet/Platform.scala
@@ -1,5 +1,11 @@
 package sjsonnet
-object Platform{
+object Platform {
+  def gzipBytes(s: Array[Byte]): String = {
+    throw new Exception("GZip not implemented in Scala.js")
+  }
+  def gzipString(s: String): String = {
+    throw new Exception("GZip not implemented in Scala.js")
+  }
   def md5(s: String): String = {
     throw new Exception("MD5 not implemented in Scala.js")
   }
