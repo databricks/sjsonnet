119228500187ea03dfe64a86748b7d2549665687
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2020-07-21 14:59:44 +0200

    Refactor scala-native Md5 for better performance

--- sjsonnet/src-native/sjsonnet/Md5.scala
+++ sjsonnet/src-native/sjsonnet/Md5.scala
@@ -8,15 +8,13 @@ object Md5 {
   // @link("crypto") // Linking using nativeLinkingOptions in build.sc instead
   @extern
   private object C {
-    def MD5(string: CString, size: CSize, result: CString): CString = extern
+    def MD5(string: Ptr[Byte], size: CSize, result: Ptr[Byte]): Ptr[Byte] = extern
   }
 
   def md5(s: String): Array[Byte] = {
     val result = ByteArray.alloc(16)
-    Zone { implicit z =>
-      val cstring = toCString(s)
-      C.MD5(cstring, strlen(cstring), result.at(0))
-    }
+    val input = s.getBytes("UTF-8")
+    C.MD5(input.asInstanceOf[ByteArray].at(0), input.length, result.at(0))
     result.asInstanceOf[Array[Byte]]
   }
 }
