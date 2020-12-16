db5759688382dc7a7c2b08ea544f27334fd9106b
Li Haoyi <haoyi.li@databricks.com>
2020-12-15 17:52:59 -0800

    swap over to java.util.Arrays binary search

--- sjsonnet/src/sjsonnet/Util.scala
+++ sjsonnet/src/sjsonnet/Util.scala
@@ -2,17 +2,17 @@ package sjsonnet
 
 
 object Util{
-  def binarySearch(lineStartsMin: Int, lineStartsMax: Int, lineStarts: Array[Int], index: Int): Int = {
-    if (lineStartsMin == lineStartsMax - 1) lineStartsMin
-    else {
-      val middle = (lineStartsMax + lineStartsMin) / 2
-      if (lineStarts(middle) > index) binarySearch(lineStartsMin, middle, lineStarts, index)
-      else binarySearch(middle, lineStartsMax, lineStarts, index)
-    }
-  }
-
   def prettyIndex(lineStarts: Array[Int], index: Int): String = {
-    val line = Util.binarySearch(0, lineStarts.length, lineStarts, index)
+    // Returns (-insertionPoint - 1) when the value is not found, where
+    // insertionPoint is the index where the element would have been inserted.
+    // Since we want to find the index of the line before the insertion point
+    // (insertionPoint - 1), we need to do a bit of math to get it
+    //
+    // (-insertionPoint - 1) = searchResult
+    // (insertionPoint + 1) = -searchResult
+    // (insertionPoint - 1) = -searchResult - 2
+    val searchResult = java.util.Arrays.binarySearch(lineStarts, index)
+    val line = if (searchResult >= 0) searchResult else -searchResult - 2
     val col = index - lineStarts(line)
     s"${line+1}:${col+1}"
   }
