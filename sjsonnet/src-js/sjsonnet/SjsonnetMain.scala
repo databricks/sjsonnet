0c975b3dfdadab60f3d05ae1b6979e552e1f1bc0
tanmay-db <tanmay.rustagi@databricks.com>
2021-09-01 13:39:09 +0200

    added mapping function

--- sjsonnet/src-js/sjsonnet/SjsonnetMain.scala
+++ sjsonnet/src-js/sjsonnet/SjsonnetMain.scala
@@ -1,5 +1,7 @@
 package sjsonnet
 
+import fastparse.Parsed
+
 import scala.collection.mutable
 import scala.scalajs.js
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
@@ -39,7 +41,16 @@ object ParseCacheCaffeine {
     // `mappingFunction` if necessary. This method provides a simple substitute for the
     // conventional "if cached, return; otherwise create, cache and return" pattern.
     // def get(key: K, mappingFunction: K => V): V = underlying.get(key, mappingFunction.asJava)
-    cache.get((path, txt))
+    cache.get((path, txt), {
+      val parsed = fastparse.parse(txt, new Parser(path).document(_)) match {
+        case f @ Parsed.Failure(_, _, _) =>
+          val traced = f.trace()
+          val pos = new Position(new FileScope(path), traced.index)
+          Left(new ParseError(traced.msg).addFrame(pos))
+        case Parsed.Success(r, _) => Right(r)
+      }
+      parsed.flatMap { case (e, fs) => process(e, fs) }
+    })
   }
 
   // Some other methods for benchmarking and profiling
