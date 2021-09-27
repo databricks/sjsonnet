13a43859f9549c28d6e1564c8ea0abad86672639
tanmay-db <tanmay.rustagi@databricks.com>
2021-09-27 10:30:32 +0200

    Fixed the JS build

--- sjsonnet/src-jvm-native/sjsonnet/SjsonnetMain.scala
+++ sjsonnet/src-jvm-native/sjsonnet/SjsonnetMain.scala
@@ -7,20 +7,6 @@ import java.nio.file.NoSuchFileException
 import scala.util.Try
 import scala.util.control.NonFatal
 
-// Trait extended by JsonnetWorker (in universe) so that it can pass the cache based on Caffeine to main0 here
-trait ParseCache {
-  def getOrElseUpdate(key: (Path, String), defaultValue:  => Either[Error, (Expr, FileScope)]): Either[Error, (Expr, FileScope)]
-}
-
-// A default implementation based on a mutable HashMap. This implementation is not thread-safe.
-class DefaultParseCache extends ParseCache {
-  val cache = new collection.mutable.HashMap[(Path, String), Either[Error, (Expr, FileScope)]]()
-
-  override def getOrElseUpdate(key: (Path, String), defaultValue:  => Either[Error, (Expr, FileScope)]): Either[Error, (Expr, FileScope)] = {
-    cache.getOrElseUpdate(key, defaultValue)
-  }
-}
-
 object SjsonnetMain {
   def createParseCache() = new DefaultParseCache
 
