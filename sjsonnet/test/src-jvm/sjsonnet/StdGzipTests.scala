9ed8b5f63bf72f64f2d30f7d6dbd5eac7ce66ebd
Stephen Amar <stephen.amar@databricks.com>
2024-12-11 22:37:39 -0800

    Run Js/Native builds in parallel

--- sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
+++ sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
@@ -11,7 +11,11 @@ object StdGzipTests extends TestSuite {
         case s if s >= 16 => "H4sIAAAAAAAA/2NkAgCSQsy2AgAAAA=="
         case _ => "H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA=="
       })
-      eval("""std.gzip("hi")""") ==> ujson.Str("H4sIAAAAAAAAAMvIBACsKpPYAgAAAA==")
+      eval("""std.gzip("hi")""") ==> ujson.Str(Runtime.version().feature() match {
+        // https://bugs.openjdk.org/browse/JDK-8244706
+        case s if s >= 16 => "H4sIAAAAAAAA/8vIBACsKpPYAgAAAA=="
+        case _ => "H4sIAAAAAAAAAMvIBACsKpPYAgAAAA=="
+      })
     }
   }
 }
