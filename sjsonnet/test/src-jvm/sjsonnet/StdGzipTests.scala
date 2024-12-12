b237858f2f05075d920aed9b751706ad5c29f29d
Stephen Amar <stephen.amar@databricks.com>
2024-12-11 22:26:27 -0800

    Fix GZIP test for java 17

--- sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
+++ sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
@@ -6,7 +6,11 @@ import utest._
 object StdGzipTests extends TestSuite {
   val tests = Tests {
     test("gzip"){
-      eval("""std.gzip([1, 2])""") ==> ujson.Str("H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA==")
+      eval("""std.gzip([1, 2])""") ==> ujson.Str(Runtime.version().feature() match {
+        // https://bugs.openjdk.org/browse/JDK-8244706
+        case s if s >= 16 => "H4sIAAAAAAAA/2NkAgCSQsy2AgAAAA=="
+        case _ => "H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA=="
+      })
       eval("""std.gzip("hi")""") ==> ujson.Str("H4sIAAAAAAAAAMvIBACsKpPYAgAAAA==")
     }
   }
