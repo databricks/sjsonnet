a982bf04c23e45b17dd8e09e79a577f08ec64bb3
Stephen Amar <stephen.amar@databricks.com>
2024-12-17 20:29:07 -0800

    add std. tests again

--- sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
+++ sjsonnet/test/src-jvm/sjsonnet/StdGzipTests.scala
@@ -6,6 +6,16 @@ import utest._
 object StdGzipTests extends TestSuite {
   val tests = Tests {
     test("gzip"){
+      eval("""std.gzip([1, 2])""") ==> ujson.Str(Runtime.version().feature() match {
+        // https://bugs.openjdk.org/browse/JDK-8244706
+        case s if s >= 16 => "H4sIAAAAAAAA/2NkAgCSQsy2AgAAAA=="
+        case _ => "H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA=="
+      })
+      eval("""std.gzip("hi")""") ==> ujson.Str(Runtime.version().feature() match {
+        // https://bugs.openjdk.org/browse/JDK-8244706
+        case s if s >= 16 => "H4sIAAAAAAAA/8vIBACsKpPYAgAAAA=="
+        case _ => "H4sIAAAAAAAAAMvIBACsKpPYAgAAAA=="
+      })
       eval("""std.native('gzip')([1, 2])""") ==> ujson.Str(Runtime.version().feature() match {
         // https://bugs.openjdk.org/browse/JDK-8244706
         case s if s >= 16 => "H4sIAAAAAAAA/2NkAgCSQsy2AgAAAA=="
--- sjsonnet/test/src-native/sjsonnet/StdGzipTests.scala
+++ sjsonnet/test/src-native/sjsonnet/StdGzipTests.scala
@@ -6,6 +6,8 @@ import utest._
 object StdGzipTests extends TestSuite {
   val tests = Tests {
     test("gzip"){
+      eval("""std.gzip([1, 2])""") ==> ujson.Str("H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA==")
+      eval("""std.gzip("hi")""") ==> ujson.Str("H4sIAAAAAAAAAMvIBACsKpPYAgAAAA==")
       eval("""std.native('gzip')([1, 2])""") ==> ujson.Str("H4sIAAAAAAAAAGNkAgCSQsy2AgAAAA==")
       eval("""std.native('gzip')("hi")""") ==> ujson.Str("H4sIAAAAAAAAAMvIBACsKpPYAgAAAA==")
     }
