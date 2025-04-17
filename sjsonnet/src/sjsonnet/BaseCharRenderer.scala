4054c776f36f512d9dface63ea71be4d839a8555
Rafał Sierkiewicz <30866803+RafalSierkiewicz@users.noreply.github.com>
2025-04-18 01:49:43 +0200

    Update dependencies  (#271)
    I wanted to split Scala 3 from updating dependencies

--- sjsonnet/src/sjsonnet/BaseCharRenderer.scala
+++ sjsonnet/src/sjsonnet/BaseCharRenderer.scala
@@ -12,6 +12,9 @@ class BaseCharRenderer[T <: upickle.core.CharOps.Output]
  indent: Int = -1,
  escapeUnicode: Boolean = false,
  newline: Array[Char] = Array('\n')) extends JsVisitor[T, T]{
+
+ override def visitJsonableObject(length: Int, index: Int): ObjVisitor[T,T] = visitObject(length, index)
+
   protected[this] val elemBuilder = new upickle.core.CharBuilder
   def flushCharBuilder(): Unit = {
     elemBuilder.writeOutToIfLongerThan(out, if (depth == 0) 0 else 1000)
@@ -154,7 +157,7 @@ class BaseCharRenderer[T <: upickle.core.CharOps.Output]
 
   private def visitNonNullString(s: CharSequence, index: Int) = {
     flushBuffer()
-    upickle.core.RenderUtils.escapeChar(null, elemBuilder, s, escapeUnicode)
+    upickle.core.RenderUtils.escapeChar(null, elemBuilder, s, escapeUnicode = escapeUnicode, wrapQuotes = true)
     flushCharBuilder()
     out
   }
