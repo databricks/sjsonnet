f4c1d576e17faf9010340a5d78e231677180142e
He-Pin <hepin1989@gmail.com>
2025-04-27 21:38:48 +0800

    feat: Add variableResolver to separate the namespaces.

--- bench/src/main/scala/sjsonnet/OptimizerBenchmark.scala
+++ bench/src/main/scala/sjsonnet/OptimizerBenchmark.scala
@@ -32,7 +32,7 @@ class OptimizerBenchmark {
     }
     this.ev = ev
     val static = inputs.map {
-      case (expr, fs) => ((new StaticOptimizer(ev, new Std().Std, mutable.HashMap.empty, mutable.HashMap.empty)).optimize(expr), fs)
+      case (expr, fs) => ((new StaticOptimizer(ev,_ => None, new Std().Std, mutable.HashMap.empty, mutable.HashMap.empty)).optimize(expr), fs)
     }
     val countBefore, countStatic = new Counter
     inputs.foreach(t => assert(countBefore.transform(t._1) eq t._1))
@@ -45,7 +45,9 @@ class OptimizerBenchmark {
   @Benchmark
   def main(bh: Blackhole): Unit = {
     bh.consume(inputs.foreach { case (expr, fs) =>
-      bh.consume((new StaticOptimizer(ev, new Std().Std, mutable.HashMap.empty, mutable.HashMap.empty)).optimize(expr))
+      bh.consume((new StaticOptimizer(ev,
+        _ => None,
+        new Std().Std, mutable.HashMap.empty, mutable.HashMap.empty)).optimize(expr))
     })
   }
 
---
+++ sjsonnet/test/src/sjsonnet/ExtFunctionNamespaceTest.scala
@@ -0,0 +1,57 @@
+package sjsonnet
+
+import utest._
+import sjsonnet.Expr.Member.Visibility
+
+object ExtFunctionNamespaceTest extends TestSuite with FunctionBuilder {
+  private val extFunctions: Map[String, Val.Func] = Map(
+    builtin("objectReplaceKey", "obj", "key", "newKey") {
+      (pos, ev, o: Val.Obj, key: String, newKey: String) =>
+        val bindings: Array[(String, Val.Obj.Member)] = for {
+          k <- o.visibleKeyNames
+          v = o.value(k, pos.fileScope.noOffsetPos)(ev)
+        } yield {
+          val newKeyName = if (k == key) newKey else k
+          (newKeyName, new Val.Obj.ConstMember(false, Visibility.Normal, v))
+        }
+        Val.Obj.mk(pos, bindings)
+    })
+
+  private val ext = Val.Obj.mk(
+    null,
+    extFunctions.toSeq.map {
+      case (k, v) => (k, new Val.Obj.ConstMember(false, Visibility.Hidden, v))
+    }: _*
+  )
+
+  private def variableResolve(name: String): Option[Expr] = {
+    if (name == "$ext" || name == "ext") {
+      Some(ext)
+    } else {
+      None
+    }
+  }
+
+  private val interpreter = new Interpreter(
+    Map(),
+    Map(),
+    DummyPath(),
+    Importer.empty,
+    parseCache = new DefaultParseCache,
+    variableResolver = variableResolve,
+  )
+
+  def check(s: String, expected: ujson.Value): Unit =
+    interpreter.interpret(s, DummyPath("(memory)")) ==> Right(expected)
+
+  def tests: Tests = Tests {
+    "test objectReplaceKey in ext namespace" - {
+      check(
+        s"""
+           |local obj = {"a": 1, "b": 2};
+           |
+           |ext.objectReplaceKey(obj, "a", "c")
+           |""".stripMargin, ujson.Obj("c" -> 1, "b" -> 2))
+    }
+  }
+}
