de5d8bfb9dda3e290a4b059c4dc25b387acd7eed
He-Pin(kerr) <hepin1989@gmail.com>
2025-04-28 13:44:05 +0800

    chore: Add val literal function test (#334)
    Add the missing test about it, I was having lunch:)
    
    refs: https://github.com/databricks/sjsonnet/pull/332

---
+++ sjsonnet/test/src/sjsonnet/ValLiteralFunctionTest.scala
@@ -0,0 +1,50 @@
+package sjsonnet
+
+import sjsonnet.Expr.Member.Visibility
+import utest._
+
+object ValLiteralFunctionTest extends TestSuite with FunctionBuilder {
+  private val extFunctions: Map[String, Val.Func] = Map(
+    builtin("identity", "obj") {
+      (_, _, o: Val.Literal) =>
+        o
+    })
+
+  private val ext = Val.Obj.mk(
+    null,
+    extFunctions.toSeq.map {
+      case (k, v) => (k, new Val.Obj.ConstMember(false, Visibility.Hidden, v))
+    }: _*
+  )
+
+  private def variableResolve(name: String): Option[Expr] = {
+    if (name == "$ext" || name == "ext") {
+      Some(ext)
+    } else {
+      None
+    }
+  }
+
+  private val interpreter = new Interpreter(
+    Map(),
+    Map(),
+    DummyPath(),
+    Importer.empty,
+    parseCache = new DefaultParseCache,
+    variableResolver = variableResolve,
+  )
+
+  def check(s: String, expected: ujson.Value): Unit =
+    interpreter.interpret(s, DummyPath("(memory)")) ==> Right(expected)
+
+  def tests: Tests = Tests {
+    "test objectReplaceKey in ext namespace" - {
+      check(
+        s"""
+           |local obj = {"a": 1, "b": 2};
+           |
+           |ext.identity(obj)
+           |""".stripMargin, ujson.Obj("a" -> 1, "b" -> 2))
+    }
+  }
+}
