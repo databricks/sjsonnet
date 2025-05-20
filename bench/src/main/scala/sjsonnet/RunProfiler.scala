7d9500b6f0e618940470a6f2e47fe7de07edf34b
Dan Stahr <danstahr@microsoft.com>
2025-05-20 13:28:52 +0200

    Add a flag to reverse the order of jpath entries.
    In the canonical implementation of jsonnet, the last match presented on
    -J wins (see
    https://jsonnet-libs.github.io/jsonnet-training-course/lesson2.html#jsonnet_path
    for details). Given the current state is behavior is documented it's
    undesirable to simply change the implementation as that breaks backwards
    compatibility so the behavior is hidden behind a feature flag.

--- bench/src/main/scala/sjsonnet/RunProfiler.scala
+++ bench/src/main/scala/sjsonnet/RunProfiler.scala
@@ -16,7 +16,7 @@ object RunProfiler extends App {
     Map.empty[String, String],
     OsPath(wd),
     importer = SjsonnetMain
-      .resolveImport(config.jpaths.map(os.Path(_, wd)).map(OsPath(_)).toIndexedSeq, None),
+      .resolveImport(config.getJpaths.map(os.Path(_, wd)).map(OsPath(_)).toIndexedSeq, None),
     parseCache = parseCache
   ) {
     override def createEvaluator(
