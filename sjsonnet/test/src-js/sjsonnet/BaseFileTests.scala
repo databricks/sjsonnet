898b7a020b044f077a92735f48dc323de2b2da1a
Stephen Amar <stephen.amar@databricks.com>
2025-06-05 09:07:40 -0700

    Enable additional math tests cases (#418)
    - Found a bug in std.mantissa/exponent by enabling those tests
    - Enabled std.native tests
    - Enabled extVar tests

--- sjsonnet/test/src-js/sjsonnet/BaseFileTests.scala
+++ sjsonnet/test/src-js/sjsonnet/BaseFileTests.scala
@@ -6,36 +6,78 @@ import utest._
 
 abstract class BaseFileTests extends TestSuite {
   private val stderr = new StringBuffer()
+  private val std = new Std(
+    additionalNativeFunctions = Map(
+      "jsonToString" -> new Val.Builtin1("jsonToString", "x") {
+        override def evalRhs(arg1: Lazy, ev: EvalScope, pos: Position): Val = {
+          Val.Str(
+            pos,
+            Materializer
+              .apply0(
+                arg1.force,
+                MaterializeJsonRenderer(indent = -1, newline = "", keyValueSeparator = ":")
+              )(ev)
+              .toString
+          )
+        }
+      },
+      "nativeError" -> new Val.Builtin0("nativeError") {
+        override def evalRhs(ev: EvalScope, pos: Position): Val =
+          Error.fail("native function error")
+      },
+      "nativePanic" -> new Val.Builtin0("nativePanic") {
+        override def evalRhs(ev: EvalScope, pos: Position): Val =
+          throw new RuntimeException("native function panic")
+      }
+    )
+  )
 
-  def importResolver(wd: Path, path: String): Option[Path] = {
+  def importResolver(
+      files: Map[String, () => Array[Byte]],
+      wd: Path,
+      path: String): Option[Path] = {
     val p = wd / path
-    if (TestResources.files.contains(p.toString)) {
+    if (files.contains(p.toString)) {
       Some(p)
     } else {
       None
     }
   }
 
-  def importLoader(path: Path, binaryData: Boolean): Either[String, Array[Byte]] =
+  def importLoader(
+      files: Map[String, () => Array[Byte]],
+      path: Path,
+      binaryData: Boolean): Either[String, Array[Byte]] =
     if (binaryData) {
-      Right(TestResources.files(path.toString))
+      Right(files(path.toString)())
     } else {
-      Left(new String(TestResources.files(path.toString), StandardCharsets.UTF_8))
+      Left(new String(files(path.toString)(), StandardCharsets.UTF_8))
     }
 
-  def eval(fileName: String, suite: String) = {
+  def eval(files: Map[String, () => Array[Byte]], fileName: String, suite: String) = {
     stderr.setLength(0)
     val interp = new Interpreter(
-      Map("var1" -> "\"test\"", "var2" -> """local f(a, b) = {[a]: b, "y": 2}; f("x", 1)"""),
+      Map(
+        "var1" -> "\"test\"",
+        "var2" -> """local f(a, b) = {[a]: b, "y": 2}; f("x", 1)""",
+        "stringVar" -> """"2 + 2"""",
+        "codeVar" -> "3 + 3",
+        "errorVar" -> "error 'xxx'",
+        "staticErrorVar" -> ")",
+        "UndeclaredX" -> "x",
+        "selfRecursiveVar" -> """[42, std.extVar("selfRecursiveVar")[0] + 1]""",
+        "mutuallyRecursiveVar1" -> """[42, std.extVar("mutuallyRecursiveVar2")[0] + 1]""",
+        "mutuallyRecursiveVar2" -> """[42, std.extVar("mutuallyRecursiveVar1")[0] + 1]"""
+      ),
       Map("var1" -> "\"test\"", "var2" -> """{"x": 1, "y": 2}"""),
       JsVirtualPath(suite),
       new Importer {
         def resolve(docBase: Path, importName: String): Option[Path] =
-          importResolver(docBase, importName)
+          importResolver(files, docBase, importName)
         def read(path: Path, binaryData: Boolean): Option[ResolvedFile] =
-          if (!TestResources.files.contains(path.toString)) None
+          if (!files.contains(path.toString)) None
           else
-            importLoader(path, binaryData) match {
+            importLoader(files, path, binaryData) match {
               case Left(s)    => Some(StaticResolvedFile(s))
               case Right(arr) => Some(StaticBinaryResolvedFile(arr))
             }
@@ -51,11 +93,12 @@ abstract class BaseFileTests extends TestSuite {
         strictImportSyntax = true,
         strictInheritedAssertions = true,
         strictSetOperations = true
-      )
+      ),
+      std = std.Std
     )
 
     interp.interpret0(
-      importLoader(JsVirtualPath(fileName), false).left.getOrElse(""),
+      importLoader(files, JsVirtualPath(fileName), false).left.getOrElse(""),
       JsVirtualPath(fileName),
       ujson.WebJson.Builder
     ) match {
@@ -64,50 +107,63 @@ abstract class BaseFileTests extends TestSuite {
     }
   }
 
-  def check(fileName: String, testSuite: String): Unit = {
+  def check(files: Map[String, () => Array[Byte]], fileName: String, testSuite: String): Unit = {
     println(s"Checking $fileName")
-    val goldenContent = if (TestResources.files.contains(fileName + ".golden_js")) {
-      new String(TestResources.files(fileName + ".golden_js"), StandardCharsets.UTF_8)
+    val goldenContent = if (files.contains(fileName + ".golden_js")) {
+      new String(files(fileName + ".golden_js")(), StandardCharsets.UTF_8)
     } else {
-      new String(TestResources.files(fileName + ".golden"), StandardCharsets.UTF_8)
+      new String(files(fileName + ".golden")(), StandardCharsets.UTF_8)
     }
     if (
       goldenContent.startsWith("sjsonnet.Error") ||
       goldenContent.startsWith("sjsonnet.ParseError") ||
       goldenContent.startsWith("sjsonnet.StaticError") ||
-      goldenContent.contains("java.lang.StackOverflowError")
+      goldenContent.contains("java.lang.StackOverflowError") ||
+      goldenContent.startsWith("RUNTIME ERROR")
     ) {
-      checkError(fileName, goldenContent, testSuite)
+      checkError(files, fileName, goldenContent, testSuite)
     } else {
-      val res = ujson.WebJson.transform(eval(fileName, testSuite), ujson.Value)
+      val res = ujson.WebJson.transform(eval(files, fileName, testSuite), ujson.Value)
       try {
         val expected = ujson.read(goldenContent)
         assert(res == expected)
         assert(stderr.toString.isEmpty)
       } catch {
         case e: ujson.ParsingFailedException =>
-          if (goldenContent.contains(stderr.toString.stripLineEnd)) {
+          if (
+            stderr.toString.stripLineEnd.nonEmpty && goldenContent.contains(
+              stderr.toString.stripLineEnd
+            )
+          ) {
             assert(true)
           } else {
+            println(res)
             assert(e.getMessage == goldenContent.stripLineEnd)
           }
       }
     }
   }
 
-  def checkError(fileName: String, goldenContent: String, testSuite: String): Unit = {
+  def checkError(
+      files: Map[String, () => Array[Byte]],
+      fileName: String,
+      goldenContent: String,
+      testSuite: String): Unit = {
     val expected = goldenContent
       .replaceAll(f"\\(sjsonnet/test/resources/$testSuite/", "(")
       .replaceAll("    at", "  at")
       .strip()
 
     try {
-      ujson.WebJson.transform(eval(fileName, testSuite), ujson.Value)
+      ujson.WebJson.transform(eval(files, fileName, testSuite), ujson.Value)
       assert(false)
     } catch {
       case e: js.JavaScriptException =>
         val msg = e.getMessage.replaceAll("<no stack trace available>", "").strip()
-        assert(msg == expected)
+        if (fileName.endsWith("native_panic.jsonnet"))
+          assert(msg.strip().contains(expected))
+        else
+          assert(msg == expected)
       case e: sjsonnet.Error =>
         assert(expected.contains(e.getMessage.strip()))
     }
--- sjsonnet/test/src-js/sjsonnet/FileTests.scala
+++ sjsonnet/test/src-js/sjsonnet/FileTests.scala
@@ -4,20 +4,20 @@ import utest._
 
 object FileTests extends BaseFileTests {
   val skippedTests = Set(
-    "test_suite/stdlib.jsonnet",
-    "test_suite/regex.jsonnet",
-    "test_suite/dos_line_endings.jsonnet",
-    "test_suite/recursive_function.jsonnet",
+    "stdlib.jsonnet",
+    "regex.jsonnet",
+    "dos_line_endings.jsonnet",
+    "recursive_function.jsonnet",
 
     // Stack size issues with the JS runner
-    "test_suite/error.array_recursive_manifest.jsonnet",
-    "test_suite/error.function_infinite_default.jsonnet",
-    "test_suite/error.obj_recursive.jsonnet",
-    "test_suite/error.obj_recursive_manifest.jsonnet",
-    "test_suite/error.recursive_object_non_term.jsonnet",
-    "test_suite/error.recursive_import.jsonnet",
-    "test_suite/error.recursive_function_nonterm.jsonnet",
-    "test_suite/error.array_large_index.jsonnet"
+    "error.array_recursive_manifest.jsonnet",
+    "error.function_infinite_default.jsonnet",
+    "error.obj_recursive.jsonnet",
+    "error.obj_recursive_manifest.jsonnet",
+    "error.recursive_object_non_term.jsonnet",
+    "error.recursive_import.jsonnet",
+    "error.recursive_function_nonterm.jsonnet",
+    "error.array_large_index.jsonnet"
   )
 
   val goTestDataSkippedTests: Set[String] = Set(
@@ -26,38 +26,17 @@ object FileTests extends BaseFileTests {
     "builtin_escapeStringJson.jsonnet",
     "builtin_manifestTomlEx.jsonnet",
     "escaped_fields.jsonnet",
-    "extvar_code.jsonnet",
-    "extvar_hermetic.jsonnet",
-    "extvar_mutually_recursive.jsonnet",
-    "extvar_not_a_string.jsonnet",
-    "extvar_self_recursive.jsonnet",
-    "extvar_static_error.jsonnet",
-    "extvar_string.jsonnet",
     "multi.jsonnet",
     "multi_string_output.jsonnet",
-    "native1.jsonnet",
-    "native2.jsonnet",
-    "native3.jsonnet",
-    "native6.jsonnet",
     "number_leading_zero.jsonnet",
     "object_comp_assert.jsonnet",
     "object_comp_bad_field.jsonnet",
     "object_comp_bad_field2.jsonnet",
     "object_comp_illegal.jsonnet",
     "pow6.jsonnet",
-    "native4.jsonnet",
-    "native5.jsonnet",
-    "native7.jsonnet",
-    "native_error.jsonnet",
-    "native_panic.jsonnet",
     "object_invariant_plus.jsonnet",
     "std.makeArray_recursive_evalutation_order_matters.jsonnet",
     "tailstrict3.jsonnet",
-    "std.exponent6.jsonnet",
-    "std.exponent7.jsonnet",
-    "std.mantissa3.jsonnet",
-    "std.mantissa6.jsonnet",
-    "std.mantissa7.jsonnet",
     "stdlib_smoke_test.jsonnet",
     "builtinSha1.jsonnet",
     "builtinSha256.jsonnet",
@@ -73,22 +52,22 @@ object FileTests extends BaseFileTests {
 
   val tests: Tests = Tests {
     test("test_suite") - {
-      val t = TestResources.files.keys.toSeq.sorted
-        .filter(f => f.matches("test_suite/[^/]+\\.jsonnet"))
+      val t = TestResources_test_suite.files.keys.toSeq.sorted
+        .filter(f => f.matches("[^/]+\\.jsonnet"))
         .filter(f => !skippedTests.contains(f))
       assert(t.nonEmpty)
       t.foreach { file =>
-        check(file, "test_suite")
+        check(TestResources_test_suite.files, file, "test_suite")
       }
     }
 
     test("go_test_suite") - {
-      val t = TestResources.files.keys.toSeq.sorted
-        .filter(f => f.matches("go_test_suite/[^/]+\\.jsonnet"))
-        .filter(f => !goTestDataSkippedTests.contains(f.substring("go_test_suite/".length)))
+      val t = TestResources_go_test_suite.files.keys.toSeq.sorted
+        .filter(f => f.matches("[^/]+\\.jsonnet"))
+        .filter(f => !goTestDataSkippedTests.contains(f))
       assert(t.nonEmpty)
       t.foreach { file =>
-        check(file, "go_test_suite")
+        check(TestResources_go_test_suite.files, file, "go_test_suite")
       }
     }
   }
