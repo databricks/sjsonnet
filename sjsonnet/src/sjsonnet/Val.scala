b2ddf098a5fe371c9dff71563846ed42d4463af7
Josh Rosen <rosenville@gmail.com>
2025-08-09 14:40:19 -0700

    asserting can be private[this]

--- sjsonnet/src/sjsonnet/Val.scala
+++ sjsonnet/src/sjsonnet/Val.scala
@@ -265,7 +265,7 @@ object Val {
       private var allKeys: util.LinkedHashMap[String, java.lang.Boolean] = null)
       extends Literal
       with Expr.ObjBody {
-    private var asserting: Boolean = false
+    private[this] var asserting: Boolean = false
 
     def getSuper: Obj = `super`
 
